697334aafc95773dc45f3033dffff95614d65bd6
IslamCabarli <icabarli@std.beu.edu.az>
2026-09-10 16:22:28 +0400

    feat: add Order model with relationships and casts

---
+++ app/Models/Order.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+
+#[Fillable(['user_id', 'order_number', 'status', 'payment_status', 'subtotal', 'discount', 'total',
+            'shipping_name', 'shipping_address', 'shipping_city', 'shipping_country', 'shipping_postal_code'])]
+
+class Order extends Model
+{
+
+    use HasFactory, SoftDeletes;
+
+    protected function casts(): array
+    {
+        return [
+            'subtotal' => 'decimal:2',
+            'discount' => 'decimal:2',
+            'total' => 'decimal:2'
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orderItems(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class);
+    }
+
+    public function couponUsages(): HasMany
+    {
+        return $this->hasMany(CouponUsages::class);
+    }
+}
