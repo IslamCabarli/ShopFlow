9f3181ac0cd5a502485560f1f318b410f584cda5
IslamCabarli <icabarli@std.beu.edu.az>
2026-09-10 15:35:20 +0400

    feat: add Category model with product relationship

---
+++ app/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+
+
+#[Fillable(['name', 'slug'])]
+
+class Category extends Model
+{
+    use HasFactory, SoftDeletes;
+
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class, 'product_categories');
+    }
+
+
+
+}
