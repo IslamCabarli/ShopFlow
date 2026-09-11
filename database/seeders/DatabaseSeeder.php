dfbc5b4ced7985d37654c3f6365723939a11e805
IslamCabarli <icabarli@std.beu.edu.az>
2026-09-11 16:51:37 +0400

    feat: seed users categories products and inventories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Inventory;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +18,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::factory()->admin()->create([
+            'email' => 'admin@shopflow.test'
         ]);
+        User::factory()->count(10)->create();
+
+        $categories = Category::factory()->count(10)->create();
+
+        Product::factory()->count(50)
+            ->create()
+            ->each(function (Product $product) use ($categories) {
+                Inventory::factory()->for($product)->create();
+                $product->categories()->attach(
+                $categories->random(rand(1,3))->pluck('id')
+                );
+            });
     }
 }
