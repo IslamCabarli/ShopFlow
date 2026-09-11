1fc732aae0414360c760cdac29d843632a5dd63a
IslamCabarli <icabarli@std.beu.edu.az>
2026-09-11 17:57:06 +0400

    feat: install Laravel Sanctum for API authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,13 +12,14 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Laravel\Sanctum\HasApiTokens;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable, SoftDeletes;
+    use HasFactory, Notifiable, SoftDeletes, HasApiTokens;
 
     /**
      * Get the attributes that should be cast.
