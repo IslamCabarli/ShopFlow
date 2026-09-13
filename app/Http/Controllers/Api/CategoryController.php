af62d71929237b7f2e1e4106e7042545ab91bf19
IslamCabarli <icabarli@std.beu.edu.az>
2026-09-13 17:26:26 +0400

    feat: add category CRUD controller

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Category\StoreCategoryRequest;
+use App\Http\Requests\Category\UpdateCategoryRequest;
+use App\Http\Resources\CategoryResource;
+use App\Http\Traits\ApiResponse;
+use App\Models\Category;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class CategoryController extends Controller
+{
+    use ApiResponse;
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): JsonResponse
+    {
+        $categories = Category::paginate(15);
+        return $this->successPaginated(
+            CategoryResource::collection($categories),
+            'Categories retrieved successfully'
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategoryRequest $request): JsonResponse
+    {
+        $data = $request->validated();
+        $data['slug'] = $data['slug'] ?? Str::slug($data['name']);
+
+        $category = Category::create($data);
+        return $this->success(
+            new CategoryResource($category),
+            'Category created successfully',
+            201
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category): JsonResponse
+    {
+        return $this->success(new CategoryResource($category));
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategoryRequest $request, Category $category): JsonResponse
+    {
+        $data = $request->validated();
+        $data['slug'] = $data['slug'] ?? $category->slug;
+
+        $category->update($data);
+
+        return $this->success(
+            new CategoryResource($category),
+            'Category updated successfully'
+        );
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category): JsonResponse
+    {
+        $category->delete();
+
+        return $this->success(
+            message: 'Category deleted successfully'
+        );
+    }
+}
