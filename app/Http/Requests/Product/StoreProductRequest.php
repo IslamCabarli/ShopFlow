e714cfe3f190add71191c83cc8c0dfebc6403b33
IslamCabarli <icabarli@std.beu.edu.az>
2026-09-14 11:06:36 +0400

    feat: add store product request

---
+++ app/Http/Requests/Product/StoreProductRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Product;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:100'],
+            'description' => ['nullable', 'string'],
+            'slug' => ['nullable', 'string', 'max:100', 'unique:products,slug'],
+            'sku' => ['required', 'string', 'max:100', 'unique:products,sku'],
+            'price' => ['required', 'numeric'],
+            'status' => ['required',Rule::in(['active', 'inactive', 'draft'])],
+            'discount_price' => ['nullable', 'numeric', 'lt:price'],
+            'category_ids' => ['nullable','array'],
+            'category_ids.*' => ['exists:categories,id'],
+        ];
+    }
+}
