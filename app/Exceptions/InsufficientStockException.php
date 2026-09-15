95e4d0829a1351608bf5e59b657e72fa27e487d8
IslamCabarli <icabarli@std.beu.edu.az>
2026-09-15 16:56:55 +0400

    feat: render insufficient stock as conflict

--- app/Exceptions/InsufficientStockException.php
+++ app/Exceptions/InsufficientStockException.php
@@ -3,5 +3,16 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
-class InsufficientStockException extends Exception{}
+class InsufficientStockException extends Exception
+{
+    public function render(Request $request):JsonResponse
+    {
+        return response()->json([
+            'message' => $this->getMessage(),
+            'errors' => [],
+        ],409);
+    }
+}
