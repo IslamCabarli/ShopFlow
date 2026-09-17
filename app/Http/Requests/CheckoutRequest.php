b141a2139a016802caf5142398b8252aad2cc8c2
IslamCabarli <icabarli@std.beu.edu.az>
2026-09-17 22:33:23 +0400

    feat: add checkout request validation

---
+++ app/Http/Requests/CheckoutRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CheckoutRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'shipping_name' => ['required', 'string', 'max:255'],
+            'shipping_address' => ['required', 'string', 'max:500'],
+            'shipping_city' => ['required', 'string', 'max:100'],
+            'shipping_country' => ['required', 'string', 'max:100'],
+            'shipping_postal_code' => ['nullable', 'string', 'max:20'],
+        ];
+    }
+}
