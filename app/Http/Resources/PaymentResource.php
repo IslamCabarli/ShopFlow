6e5fd8614b851e40e4fcb200a7cdd96010453ad8
IslamCabarli <icabarli@std.beu.edu.az>
2026-09-19 14:43:42 +0400

    feat: add payment API resource

---
+++ app/Http/Resources/PaymentResource.php
@@ -0,0 +1,20 @@
+<?php
+
+    namespace App\Http\Resources;
+
+    use Illuminate\Http\Request;
+    use Illuminate\Http\Resources\Json\JsonResource;
+
+    class PaymentResource extends JsonResource
+    {
+        public function toArray(Request $request): array
+        {
+            return [
+                'id' => $this->id,
+                'status' => $this->status,
+                'provider' => $this->provider,
+                'amount' => $this->amount,
+                'paid_at' => $this->paid_at,
+            ];
+        }
+    }
